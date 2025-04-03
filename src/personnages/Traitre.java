c0841da6b8588bebada77d3029c29c4a0712a4df
Maledict <villemurclement@gmail.com>
2025-04-03 15:27:49 +0200

    Fin de la journée youjhqzeiufiuqe

---
+++ src/personnages/Traitre.java
@@ -0,0 +1,11 @@
+package personnages;
+
+public class Traitre extends Samourai {
+	
+	private int niveauTraitrise;
+
+	public Traitre(String seigneur, String nom, String boissonFav, int argent) {
+		super(seigneur, nom, boissonFav, argent);
+	}
+
+}

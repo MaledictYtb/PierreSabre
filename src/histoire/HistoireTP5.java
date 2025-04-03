1786edc5b3402c7d1cae4e501f895aeee984a7a6
Maledict <villemurclement@gmail.com>
2025-04-03 15:09:30 +0200

    les samourais

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -2,6 +2,7 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Ronin;
+import personnages.Samourai;
 import personnages.Yakuza;
 
 public class HistoireTP5 {
@@ -22,6 +23,13 @@ public class HistoireTP5 {
 		marco.listerConnaissances();
 		roro.listerConnaissances();
 		yaku.listerConnaissances();
+		
+		System.out.println("------------- LES SAMOURAI ---------------");
+		
+		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
+		akimoto.faireConnaissanceAvec(marco);
+		akimoto.listerConnaissances();
+		akimoto.boire("thé");
 	}
 
 }
